c8e35e4ce9ae948a14c31491cb2fa5e3d4affec1
zairlaka <zair.100.pro@gmail.com>
2021-08-07 22:21:30 +0500

    mail functionality done letter opener for development ENV

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jsonapi.rb'
 # logs all changes to your models
 gem "audited", "~> 4.9"
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -35,6 +36,8 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'letter_opener'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,27 @@
 class UsersController < ApplicationController
   before_action :load_user, only: %i[archive unarchive]
 
-
+  # http://localhost:3000/users?keyword=archive / keyword=unarchive
   def index
     check = params[:keyword] == "archive" ? true : false
     user = params[:keyword].present? ? User.where(archive: check) : User.all
     render jsonapi: user
   end
 
+  # http://localhost:3000/users/:id/archive
   def archive
     if @user.update_attribute :archive, true
+      UserMailer.inform_email(@user,current_user).deliver
       render json: { success: true, message: 'User Archived Successfully'}, status: :ok
     else
       render json: { success: false, message: 'Errors', errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
+  # http://localhost:3000/users/:id/unarchive
   def unarchive
     if @user.update_attribute :archive, false
-      
+      UserMailer.inform_email(@user,current_user).deliver
       render json: { success: true, message: 'User Unarchived Successfully'}, status: :ok
     else
       render json: { success: false, message: 'Errors', errors: @user.errors.full_messages }, status: :unprocessable_entity
